a2b74550462eae3c0ace3bed91baf2f9014cdbc9
antoniomuso <antoniomusolino007@gmail.com>
2019-01-10 11:19:42 +0100

    risolto problema di parsing

--- serverRapido/index.js
+++ serverRapido/index.js
@@ -6,8 +6,7 @@ const NEW_LINE = 10
 const COMMA = 44
 
 var server = net.createServer(function(c) { //'connection' listener
-  let data_buff = []
-  let inRead = false
+
   console.log('server connected');
   c.on('end', function() {
     console.log('server disconnected');
@@ -15,20 +14,19 @@ var server = net.createServer(function(c) { //'connection' listener
 
   let file_name = Date.now().toString() + ".json"
   let writeStream = fs.createWriteStream(`out/${file_name}`)
+  writeStream.write("[")
   
-  c.pipe(th2((chunk, enc, callback) => {
-    chunk = Buffer.concat([Buffer.from("["), chunk, Buffer.from("]")])
+  c.pipe(th2((chunk, enc, callback) => {  
     for (let i = 0; i < chunk.length; i++) {
       if (chunk[i] == NEW_LINE) {
         chunk[i] = COMMA
       }
     }
     callback(null, chunk)
-  })).pipe(writeStream)
+  })).pipe(writeStream).addListener('finish',() => fs.appendFile(`out/${file_name}`,"]",(err) => {
+    if (err) console.err(`File name ${file_name} error: ${err}`)
+  }))
 
-  
-  c.write('hello \r\n');
-  //c.pipe(c);
 });
 server.listen(8080, function() { //'listening' listener
   console.log('server bound');
