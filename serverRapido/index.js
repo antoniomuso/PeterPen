cf083c0c80aa81ebab800420364ca603869b8ac7
antoniomuso <antoniomusolino007@gmail.com>
2019-01-11 13:09:39 +0100

    aggiunto troncamento dell ultima virgola del json adesso il formato ottenuto è corretto

--- serverRapido/index.js
+++ serverRapido/index.js
@@ -1,6 +1,7 @@
 let net = require('net')
 let fs = require('fs')
 let th2 = require('through2')
+let trunc_file = require('./lib/truncate_last')
 
 const NEW_LINE = 10
 const COMMA = 44
@@ -13,12 +14,15 @@ var server = net.createServer(function(c) { //'connection' listener
   })
   
   let file_name = Date.now().toString() + ".json"
+  let file_path = `out/${file_name}`
   let writeStream = fs.createWriteStream(`out/${file_name}`)
   writeStream.write("[")
 
   c.setTimeout(1000 * 60 * 3,() => {
-    fs.appendFile(`out/${file_name}`,"]",(err) => {
-      if (err) console.err(`File name ${file_name} error: ${err}`)
+    trunc_file(file_path, 1, () => {
+      fs.appendFile(file_path,"]",(err) => {
+        if (err) console.err(`File name ${file_name} error: ${err}`)
+      })
     })
     c.unpipe()
     c.end()
